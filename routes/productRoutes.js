424ff8a1e4e79dd263bf24b61cc128ca700ff139
iliav <iliav@hurdit.com>
2019-04-08 23:46:13 -0400

    HTML template stuff added, added test JSON parsing as well

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,17 @@
+const request = require('request');
+
 module.exports = app => {
   app.get('/api/products', (req, res) => {
-    res.json({ test: true });
+    // res.json({ test: true });
+    request('https://next.json-generator.com/api/json/get/EkzBIUWNL', function(
+      error,
+      response,
+      body
+    ) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+      res.send(JSON.parse(body));
+    });
   });
 };
